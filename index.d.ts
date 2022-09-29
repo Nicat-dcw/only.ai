12315fab9c54599c860c59e62e0190de64877d78
Davut Özgür Süküti <80099012+davutozgursukuti4531@users.noreply.github.com>
2022-09-29 16:29:43 +0300

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,10 @@
 import EventEmitter from "node:events"
 declare module "only.ai"{
     export default class ai {
-        public ai(soru: string) : void;
+    public constructor({ accessToken: string, newUser: string }) 
+    Sor: require("./main.js");
+    releases: require("./release.js")
+    ekle: require("./kelimeEkle.js")
+    set: require("./set.js")
     }
 }

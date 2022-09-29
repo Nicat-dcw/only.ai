3cfac007895720a02518860469b30011313dcb3f
Davut Özgür Süküti <80099012+davutozgursukuti4531@users.noreply.github.com>
2022-09-29 16:32:30 +0300

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,13 @@
 import EventEmitter from "node:events"
 declare module "only.ai"{
+    export type OnlyAISoruEvents = {
+    hata: (hata: Error) => void | Promise<void>
+    }
     export default class ai {
     public constructor({ accessToken: string, newUser: string }) 
-    Sor: require("./main.js");
+        Sor: class Sor extends (EventEmitter as new() => TypedEventEmitter<OnlyAISoruEvents>){
+           async sor(soru, ayarlar, configs = {})
+};
     releases: require("./release.js")
     ekle: require("./kelimeEkle.js")
     set: require("./set.js")
